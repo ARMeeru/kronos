ac7250c07cdfeccd67411b342348dac16817c407
Asifur Rahaman <asifurrahaman@Meerus-iMac.local>
2023-03-02 09:08:57 +0600

    feat: concurrency implemented

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Kronos
+# 1. Kronos
 Kronos is a simple command-line utility to test the response time of HTTP APIs. You can specify the API method, URL, headers, request body, and the acceptable response time threshold, and Kronos will make the HTTP request and report whether the response time was within the threshold.
-## Usage
+## 1.1. Usage
 Build kronos using the following command
 
     go build -o kronos main.go
@@ -10,24 +10,17 @@ Ask for available flags
     ./kronos -help
 
 Get the program running
-
 `./kronos -csv api_info.csv` OR `./kronos -csv api_info.csv -threshold 3`
 
 Sample response
 
-    Response time for GET https://api.ipify.org: 1.978596s
-    
-    Test passed for GET https://api.ipify.org.
-    
-    Response time for POST https://reqbin.com/echo/post/json: 38.443ms
-    
-    Test passed for POST https://reqbin.com/echo/post/json.
+    Request to https://reqbin.com/echo/post/json completed in 152.280167ms
+    Request to https://api.ipify.org took too long (1.251576584s)
 
-## Contributing
+## 1.2. Contributing
 If you find a bug or have a feature request, please open an issue on GitHub. Pull requests are also welcome.
-## To improve
-- Error handling
-
--  ~~User feedback~~
-
-- ~~Testing~~
+## 1.3. To improve
+- Implement concurrency
+- ~~Error handling~~
+- ~~User feedback~~
+- Testing (files will be added properly later)
--- main.go
+++ main.go
@@ -1,141 +1,163 @@
 package main
 
 import (
+	"bytes"
 	"encoding/csv"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
+	"sync"
 	"time"
 )
 
-// The client field specifies the settings for the HTTP client, including the maximum time allowed for a request to complete.
-type Kronos struct {
-	client *http.Client
-}
-
-type APIInfo struct {
-	Method string
-	URL    string
-	// The Headers field is a map that associates string keys with string values
-	Headers     map[string]string
-	RequestBody string
-}
-
-// By returning a pointer here, the function ensures that any changes made to the Kronos instance will be reflected in the original instance, rather than creating a copy of the instance.
-func NewKronos() *Kronos {
-	return &Kronos{
-		client: &http.Client{Timeout: 5 * time.Second},
-	}
-}
-
-func (k *Kronos) Run(apiInfo *APIInfo, threshold time.Duration) {
-	req, err := http.NewRequest(apiInfo.Method, apiInfo.URL, nil)
-	if err != nil {
-		fmt.Printf("Error creating HTTP request for %s %s: %v\n", apiInfo.Method, apiInfo.URL, err)
-		return
-	}
-
-	for key, value := range apiInfo.Headers {
-		req.Header.Set(key, value)
-	}
-
-	if apiInfo.RequestBody != "" {
-		req.Body = ioutil.NopCloser(strings.NewReader(apiInfo.RequestBody))
-	}
-
-	start := time.Now()
-	resp, err := k.client.Do(req)
-	elapsed := time.Since(start)
-
-	if err != nil {
-		fmt.Printf("Error sending HTTP request for %s %s: %v\n", apiInfo.Method, apiInfo.URL, err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	fmt.Printf("Response time for %s %s: %v\n", apiInfo.Method, apiInfo.URL, elapsed)
-
-	if elapsed > threshold {
-		fmt.Printf("Test failed: response time for %s %s exceeded threshold of %v.\n", apiInfo.Method, apiInfo.URL, threshold)
-	} else {
-		fmt.Printf("Test passed for %s %s.\n", apiInfo.Method, apiInfo.URL)
-	}
-}
-
-func readAPIInfoFromCSV(filename string) ([]*APIInfo, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	r := csv.NewReader(file)
-	var apiInfos []*APIInfo
-
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		apiInfo := &APIInfo{
-			Method:      record[0],
-			URL:         record[1],
-			Headers:     parseKeyValueString(record[2]),
-			RequestBody: record[3],
-		}
-
-		apiInfos = append(apiInfos, apiInfo)
-	}
-
-	return apiInfos, nil
-}
-
-func parseKeyValueString(s string) map[string]string {
-	headers := make(map[string]string)
-	headerPairs := strings.Split(s, ",")
-	for _, pair := range headerPairs {
-		pairParts := strings.Split(pair, "=")
-		if len(pairParts) == 2 {
-			headers[pairParts[0]] = pairParts[1]
-		}
-	}
-	return headers
-}
-
 func main() {
-	csvFilename := flag.String("csv", "", "CSV file containing API info")
+	// Parse command-line arguments
+	csvFilename := flag.String("csv", "api_info.csv", "CSV file containing API info")
 	thresholdStr := flag.String("threshold", "1", "Acceptable response time threshold (in seconds)")
 	flag.Parse()
 
-	if *csvFilename == "" {
-		fmt.Println("Error: CSV file path cannot be empty")
+	// Parse the threshold value
+	threshold, err := time.ParseDuration(*thresholdStr + "s")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Invalid threshold value: %v\n", err)
+		threshold, _ = time.ParseDuration("1s") // Default threshold of 1 second
+	}
+
+	// Open the CSV file
+	csvFile, err := os.Open(*csvFilename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening CSV file: %v\n", err)
+		os.Exit(1)
+	}
+	defer csvFile.Close()
+
+	// Parse the CSV data
+	apiData, err := parseCsvData(csvFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing CSV data: %v\n", err)
 		os.Exit(1)
 	}
 
-	apiInfos, err := readAPIInfoFromCSV(*csvFilename)
-	if err != nil {
-		fmt.Println("Error reading API info from CSV:", err)
-		os.Exit(1)
+	// Set up a worker pool to process API requests concurrently
+	numWorkers := 5
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+	apiDataChan := make(chan *apiInfo)
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			defer wg.Done()
+
+			// Loop through the API data channel and make requests
+			for apiData := range apiDataChan {
+				// Make the API request
+				start := time.Now()
+				resp, err := makeRequest(apiData)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Error making request: %v\n", err)
+					continue
+				}
+				responseTime := time.Since(start)
+
+				// Check the response time
+				if responseTime > threshold {
+					fmt.Fprintf(os.Stderr, "Request to %s took too long (%v)\n", apiData.Url, responseTime)
+					os.Exit(1)
+				} else {
+					fmt.Printf("Request to %s completed in %v\n", apiData.Url, responseTime)
+				}
+
+				// Close the response body
+				_, _ = io.Copy(ioutil.Discard, resp.Body)
+				resp.Body.Close()
+			}
+		}()
 	}
 
-	threshold, err := strconv.Atoi(*thresholdStr)
+	// Push the API data to the channel
+	for _, data := range apiData {
+		apiDataChan <- data
+	}
+	close(apiDataChan)
+
+	// Wait for all workers to finish
+	wg.Wait()
+
+}
+
+// Structure to hold API information
+type apiInfo struct {
+	Method string
+	Url    string
+	Body   string
+	Header http.Header
+}
+
+// Parse CSV data into an array of API info structs
+func parseCsvData(csvFile io.Reader) ([]*apiInfo, error) {
+	var apiData []*apiInfo
+
+	// Parse the CSV data
+	csvReader := csv.NewReader(csvFile)
+	csvData, err := csvReader.ReadAll()
 	if err != nil {
-		fmt.Println("Invalid threshold value, using default value of 1 seconds")
-		threshold = 3
+		return nil, err
 	}
 
-	k := NewKronos()
-	for _, apiInfo := range apiInfos {
-		k.Run(apiInfo, time.Duration(threshold)*time.Second)
+	// Get the headers from the CSV data
+	headers := csvData[0]
+
+	// Loop through the rows in the CSV data
+	for i, row := range csvData[1:] {
+		// Check that the row has the correct number of columns
+		if len(row) != len(headers) {
+			return nil, errors.New(fmt.Sprintf("Incorrect number of columns in row %d", i+1))
+		}
+
+		// Create a new API info struct
+		info := &apiInfo{
+			Method: row[0],
+			Url:    row[1],
+			Body:   row[2],
+			Header: make(http.Header),
+		}
+
+		// Parse the headers
+		for j := 3; j < len(headers); j++ {
+			headerName := headers[j]
+			headerValue := row[j]
+			if headerValue != "" {
+				info.Header.Add(headerName, headerValue)
+			}
+		}
+
+		// Add the API info to the array
+		apiData = append(apiData, info)
 	}
+
+	return apiData, nil
+}
+
+// Make an HTTP request and return the response and an error, if any
+func makeRequest(apiData *apiInfo) (*http.Response, error) {
+	// Create the request
+	req, err := http.NewRequest(apiData.Method, apiData.Url, bytes.NewBufferString(apiData.Body))
+	if err != nil {
+		return nil, err
+	}
+
+	// Add the headers to the request
+	req.Header = apiData.Header
+
+	// Make the request
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
